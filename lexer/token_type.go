5127ff178f9057171973b8c90404a1be352e3486
John Loyd Mulit <jlmulit68@gmail.com>
2023-12-16 15:53:15 +0800

    refactor: change TokenType from int64 to string type

--- lexer/token_type.go
+++ lexer/token_type.go
@@ -1,51 +1,53 @@
 package lexer
 
+type TokenType string
+
 const (
 	// single-character tokens
-	LEFT_PAREN  = iota
-	RIGHT_PAREN = iota
-	LEFT_BRACE  = iota
-	RIGHT_BRACE = iota
-	COMMA       = iota
-	DOT         = iota
-	MINUS       = iota
-	PLUS        = iota
-	SEMICOLON   = iota
-	SLASH       = iota
-	STAR        = iota
+	LEFT_PAREN  TokenType = "LEFT_PAREN"
+	RIGHT_PAREN TokenType = "RIGHT_PAREN"
+	LEFT_BRACE  TokenType = "LEFT_BRACE"
+	RIGHT_BRACE TokenType = "RIGHT_BRACE"
+	COMMA       TokenType = "COMMA"
+	DOT         TokenType = "DOT"
+	MINUS       TokenType = "MINUS"
+	PLUS        TokenType = "PLUS"
+	SEMICOLON   TokenType = "SEMICOLON"
+	SLASH       TokenType = "SLASH"
+	STAR        TokenType = "STAR"
 
 	// one or two character tokens
-	BANG          = iota
-	BANG_EQUAL    = iota
-	EQUAL         = iota
-	EQUAL_EQUAL   = iota
-	GREATER       = iota
-	GREATER_EQUAL = iota
-	LESS          = iota
-	LESS_EQUAL    = iota
+	BANG          TokenType = "BANG"
+	BANG_EQUAL    TokenType = "BANG_EQUAL"
+	EQUAL         TokenType = "EQUAL"
+	EQUAL_EQUAL   TokenType = "EQUAL_EQUAL"
+	GREATER       TokenType = "GREATER"
+	GREATER_EQUAL TokenType = "GREATER_EQUAL"
+	LESS          TokenType = "LESS"
+	LESS_EQUAL    TokenType = "LESS_EQUAL"
 
 	// literals
-	IDETIFIER = iota
-	STRING    = iota
-	NUMBER    = iota
+	IDETIFIER TokenType = "IDETIFIER"
+	STRING    TokenType = "STRING"
+	NUMBER    TokenType = "NUMBER"
 
 	// keywords
-	AND    = iota
-	CLASS  = iota
-	ELSE   = iota
-	FALSE  = iota
-	FUN    = iota
-	FOR    = iota
-	IF     = iota
-	NIL    = iota
-	OR     = iota
-	PRINT  = iota
-	RETURN = iota
-	SUPER  = iota
-	THIS   = iota
-	TRUE   = iota
-	VAR    = iota
-	WHILE  = iota
+	AND    TokenType = "AND"
+	CLASS  TokenType = "CLASS"
+	ELSE   TokenType = "ELSE"
+	FALSE  TokenType = "FALSE"
+	FUN    TokenType = "FUN"
+	FOR    TokenType = "FOR"
+	IF     TokenType = "IF"
+	NIL    TokenType = "NIL"
+	OR     TokenType = "OR"
+	PRINT  TokenType = "PRINT"
+	RETURN TokenType = "RETURN"
+	SUPER  TokenType = "SUPER"
+	THIS   TokenType = "THIS"
+	TRUE   TokenType = "TRUE"
+	VAR    TokenType = "VAR"
+	WHILE  TokenType = "WHILE"
 
-	EOF = iota
+	EOF TokenType = "EOF"
 )
