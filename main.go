c9ee4994b3c3d95d68f36ee3098b51be93ad884a
John Loyd Mulit <jlmulit68@gmail.com>
2023-12-19 13:34:07 +0800

    feat: can now check version

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/BeepLoop/bai-interpreter/lexer"
+import (
+	"fmt"
+	"os"
+
+	"github.com/BeepLoop/bai-interpreter/lexer"
+)
+
+var version = "1.0.0"
 
 func main() {
+	args := os.Args
+
+	if len(args) > 1 {
+
+		switch args[1] {
+		case "-v":
+			fmt.Println(version)
+			return
+
+		case "--version":
+			fmt.Println(version)
+			return
+		}
+	}
+
 	lexer.Lexer()
 }
